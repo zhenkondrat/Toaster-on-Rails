44627e65b079cc86103b5e35e2e68eaae64410e8
Kris <elhowm@gmail.com>
2014-09-21 14:38:46 +0300

    Admin&User cabinet

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 Rails.application.routes.draw do
+  resources :tests
+
+  resources :subjects
+
   # resources :users
 
   devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
+  get 'journal' => 'journal#index'
   # You can have the root of your site routed with "root"
   root 'users#index'
 

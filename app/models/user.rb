0d131f49927509bf93280c6b93f90ba079a4341d
Kris <elhowm@gmail.com>
2014-10-12 16:03:11 +0300

    Links for tests in student cabinet

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,11 @@
 class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable, :rememberable
   has_many :groups, through: :user_groups
+
   def email_required?
     false
   end
+
   def registrations
     groups_id = ""
     groups = UserGroup.where('user_id' => self.id).select(:group_id)
@@ -12,4 +14,12 @@ class User < ActiveRecord::Base
     }
     groups_id.empty? ? nil : Group.where('id IN ('+groups_id.chop.chop+')')
   end
+
+  def tests
+    Test.joins('INNER JOIN test_groups ON test_groups.test_id = tests.id
+                INNER JOIN user_groups ON test_groups.group_id = user_groups.group_id
+                INNER JOIN users ON users.id = '+self.id.to_s)
+              .select('tests.id, tests.name')
+  end
 end
+

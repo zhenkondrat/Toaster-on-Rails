fe85aaa5550076cb4c96418396ededba2b834847
Kris <elhowm@gmail.com>
2015-01-12 00:36:32 +0200

    Group model: refactored & tested

---
+++ spec/models/group_spec.rb
@@ -0,0 +1,15 @@
+require 'spec_helper'
+
+describe Group do
+
+  let(:group) {FactoryGirl.create(:group)}
+
+  describe 'validates' do
+    it { expect(group).to validate_presence_of(:name) }
+  end
+
+  describe 'associations' do
+    it { expect(group).to have_many(:user) }
+    it { expect(group).to have_many(:test) }
+  end
+end

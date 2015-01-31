2e02eabdd8ab3497c442e3a2f673c739e51ceb9b
Kris <elhowm@gmail.com>
2015-01-31 23:00:59 +0200

    fix models

--- app/models/answer2.rb
+++ app/models/answer2.rb
@@ -1,5 +1,5 @@
 class Answer2 < ActiveRecord::Base
   belongs_to :question
-  validates :question, :answer, presence: true
-  validates_inclusion_of :is_right, in: [true, false], allow_nil: false
+  validates :question, :text, presence: true
+  validates_inclusion_of :is_right, in: [true, false]
 end

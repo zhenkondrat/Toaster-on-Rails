57bc3be9c1a17785e012bd9c4896bd23cd71c6dd
Kris <elhowm@gmail.com>
2015-06-06 23:13:09 +0300

    revert style feture

--- app/models/association.rb
+++ app/models/association.rb
@@ -10,6 +10,6 @@ class Association < ActiveRecord::Base
   private
 
   def some_side_present?
-    errors.add(:sides, %q(Can't be empty in one time)) if left_text.blank? && right_text.blank?
+    errors.add(:sides, %q|Can't be empty in one time|) if left_text.blank? && right_text.blank?
   end
 end

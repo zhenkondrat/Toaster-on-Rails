ed0c3f0af7fc8940d7b85e3e6f2541932f506e7f
Kris <elhowm@gmail.com>
2015-06-06 23:26:44 +0300

    refactoring controller with rubocop

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -17,11 +17,10 @@ class ResultsController < ApplicationController
   def export
     @users = User.joins(:groups, :results).where(groups: {id: params[:group][:id]}, results: {toast_id: params[:toast][:id]}).group('users.id').order(last_name: :asc)
 
-    file_name = "#{Group.find(params[:group][:id]).name}-result-#{Time.zone.now.strftime("%d/%m/%Y")}"
+    file_name = "#{Group.find(params[:group][:id]).name}-result-#{Time.zone.now.strftime('%d/%m/%Y')}"
     respond_to do |format|
       format.xls { set_header('xls', "#{file_name}.xls") }
       format.doc { set_header('doc', "#{file_name}.doc") }
     end
   end
-
 end

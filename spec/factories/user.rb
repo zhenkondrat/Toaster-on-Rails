034b20894ff3b21705527e8af5f680d393a42c9a
Kris <elhowm@gmail.com>
2015-09-06 13:31:33 +0300

    group_spec done

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -1,6 +1,6 @@
 FactoryGirl.define do
   factory :user do
-    login Faker::Internet.user_name
+    sequence(:login) { Faker::Internet.user_name }
     password Faker::Lorem.characters(5)
     last_name Faker::Name.last_name
   end

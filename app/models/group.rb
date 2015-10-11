0ff20deb3fc63fd3532bcb531da3839e34a3ba93
Kris <elhowm@gmail.com>
2015-10-11 17:40:58 +0300

    group model is done

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,11 +1,15 @@
 class Group < ActiveRecord::Base
   has_and_belongs_to_many :users
-  has_and_belongs_to_many :subjects
   has_and_belongs_to_many :toasts
 
   validates :name, presence: true
 
   def foreign_users
     User.where.not(id: users)
+  end
+
+  def add_users(users)
+    manager = InsertHelper.new(table_name: 'groups_users')
+    manager.insert!(cols: [:group_id, :user_id], rows: users.map{|u| [id, u.id]})
   end
 end

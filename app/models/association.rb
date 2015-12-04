80aa5d19aa5acfcca30cac6935ea50f6541ec8f9
Kris <elhowm@gmail.com>
2015-12-04 18:14:30 +0200

    association model is ready

--- app/models/association.rb
+++ app/models/association.rb
@@ -1,15 +1,19 @@
+# Answer type
 class Association < ActiveRecord::Base
   belongs_to :question
 
   validate :some_side_present?
+  validates :question, presence: true
 
-  def correct_pair?
-    left_text.present? || right_text.present?
+  def full_pair?
+    left_text.present? && right_text.present?
   end
 
   private
 
   def some_side_present?
-    errors.add(:sides, %q|Can't be empty in one time|) if left_text.blank? && right_text.blank?
+    if left_text.blank? && right_text.blank?
+      errors.add(:sides, %q|Can't be empty in one time|)
+    end
   end
 end
--- spec/models/association_spec.rb
+++ spec/models/association_spec.rb
@@ -1,11 +1,7 @@
 require 'spec_helper'
 
 describe Association do
-  let(:mark_system) {FactoryGirl.create(:mark_system)}
-  let(:subject) {FactoryGirl.create(:subject)}
-  let(:toast) {FactoryGirl.create(:toast, mark_system: mark_system, subject: subject)}
-  let(:question) {FactoryGirl.create(:question, toast: toast, question_type: 3)}
-  let(:association) {FactoryGirl.create(:association, question: question)}
+  let(:association) { create(:association) }
 
   describe 'associations' do
     it { expect(association).to belong_to(:question) }
@@ -14,26 +10,20 @@ describe Association do
   describe 'validates' do
     it { expect(association).to validate_presence_of(:question) }
 
-    context '#correct_pair?'
-      it { expect(association.correct_pair?).to eq true }
-
-      it 'should return false if it is not correct pair' do
-        association.left_text = nil
-        expect(association.correct_pair?).to eq false
-      end
-    end
-
     context '#some_side_present?' do
-      it %q|two sides can't be empty in one time| do
-        association.left_text = nil
-        association.right_text = ''
-        expect(association.valid?).to eq false
-      end
+      let(:invalid_association) { build(:association, left_text: nil, right_text: '') }
+      let(:partial_association) { create(:association, left_text: nil) }
 
-      it %q|one side can be empty| do
-        association.left_text = nil
-        association.right_text = 'blabla'
-        expect(association.valid?).to eq true
-      end
+      it { expect(association).to be_valid }
+      it { expect(partial_association).to be_valid }
+      it { expect(invalid_association).to be_invalid }
     end
+  end
+
+  context '#full_pair?' do
+    let(:partial_association) { create(:association, left_text: nil) }
+
+    it { expect(association.full_pair?).to eq true }
+    it { expect(partial_association.full_pair?).to eq false }
+  end
 end

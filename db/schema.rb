eee65cfe6ac5fee771089f079591409c0f47c053
Kris <elhowm@gmail.com>
2015-12-06 02:53:48 +0200

    plural model is ready & user now have email

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151204181649) do
+ActiveRecord::Schema.define(version: 20151206005006) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -68,7 +68,7 @@ ActiveRecord::Schema.define(version: 20151204181649) do
   create_table "plurals", force: :cascade do |t|
     t.integer "question_id"
     t.string  "text"
-    t.boolean "is_right"
+    t.boolean "is_right",    default: false
   end
 
   create_table "questions", force: :cascade do |t|
